99becbfe88380fb0233e467cb3cba5c64fc10d1c
Nouamanezh909 <nouamanezh99@gmail.com>
2020-04-12 14:52:34 +0100

    scrapping data from api

--- run.py
+++ run.py
@@ -1,19 +1,12 @@
 from flask import Flask,render_template
 from main.moviesty import stream
 
-
 # Global app initalization 
-app = Flask(__name__, template_folder='main/Templates',static_folder='main/static')
+app = Flask(__name__, template_folder='main/Templates',
+static_folder='main/static')
 
-# Register apps below 
+# # Register apps below 
 app.register_blueprint(stream)
-
-# run global app 
-@app.route("/")
-def home():
-    return render_template(
-        'index.html'  
-    )
 
 if __name__ == "__main__":
     app.run(debug=True)
