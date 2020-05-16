027410ff73e7fb7ffc492052abd89174d7ded73f
artchiee <53340008+artchiee@users.noreply.github.com>
2020-05-16 15:38:47 +0100

    fetch by category is working

--- main/moviesty.py
+++ main/moviesty.py
@@ -53,25 +53,25 @@ def main():
 
     trending_path = str(global_url + word + media_type + time_window + key_word + api_key)
     request_url = requests.get(trending_path)
-    url_to_json =request_url.json()
+    trend_jsn =request_url.json()
 
     if request_url:
         r = request_url.status_code
-        trending_file = 'Trending_movies.json'  #dave to * local file 
+        trending_file = 'Trending_movies.json'  #save to * local file 
         print('trending url response \t  : ', r)
+
         with open(trending_file, 'w') as w:
-            json.dump(url_to_json,w, indent=4)
+            json.dump(trend_jsn, w, indent=4)
         
-        # read the data 
-        with open(trending_file, 'r') as rd:
-            trnd = json.load(rd)
-            trn_clean = json.dumps(trnd, indent=4)  #optional 
-           # print('Trending data ; \n ', trn_clean)
     else:
         Exception()
- 
+    
+    # fun attribute 
+    main.trend_data = trend_jsn
+    
+    
     # carousel not working yet 
-    data_slide = trnd['results'][0:3]
+    data_slide = main.trend_data['results'][0:3]
  
     # popular Logic
     url_popular = 'movie/popular'
@@ -80,6 +80,7 @@ def main():
 
     popular_list = requests.get(url)
     resp = popular_list.status_code 
+    to_json = popular_list.json()
 
     # Check reqponese is valid or not (optional)
     if resp:
@@ -87,49 +88,22 @@ def main():
     else:
         Exception()
     
-    # converte url data to json
-    to_json = popular_list.json()
-
+    # converte url data to json / create fun attribut
+    main.pop_dt = to_json
+    
+   
     # save to a local file 
     with open('Popular_movies.json', 'w') as save_dt:
         json.dump(to_json, save_dt, indent=4)
 
-    # Must read the data to display it in jinja
-    with open('Popular_movies.json', 'r') as f:
-        data = json.load(f)
-
 
     return render_template(
         'popular_mv.html',
-        popular_movies = data,  
-        trend_img = trnd,
+        popular_movies = main.pop_dt,  
+        trend_dt = main.trend_data,
         data_slide = data_slide
     )
 
-# # genres list names 
-# @stream.context_processor
-# def get_genres():
-    
-#     genre_base = 'genre/movie/list'
-#     genres_url = str(global_url + genre_base + key_word + api_key)
-#     genre_req  = requests.get(genres_url)
-
-#     if genre_req:
-#         print('genres response was :  ',  genre_req.status_code)
-#         genre_to_json = genre_req.json()
-#         #dump_gnr = json.dumps(genre_to_json, indent=4) 
-
-#         # save to jsn file 
-#         with open('genres.json', 'w') as gnrs:
-#             json.dump(genre_to_json, gnrs, indent=4, sort_keys=True)
-
-#     else:
-#         Exception()
-
-#     # alter ids to be keys in genre list 
-#     return dict (
-#         key= 'value'
-#     )
 
 @stream.route('/fetch', methods=['POST', 'GET'])
 def search_by():
@@ -153,11 +127,12 @@ def search_by():
 
                 print('Found movie results for %s ' % query_str, '\n',
                 'Global results are %d \n' % len(search_dt['results']) )
-                
+
+                #optional
                 for get_dt in search_dt['results']:
                     titles = get_dt['title']
                     if titles:
-                        print('--',json.dumps(titles, indent=4))
+                        print('--',json.dumps(titles, indent=4)) 
 
                     else:
                         Exception()
@@ -185,16 +160,103 @@ def search_by():
         
     )
 
+# # genres list names 
+@stream.context_processor
+def get_genres():
+    
+    # genre_base = 'genre/movie/list'
+    # genres_url = str(global_url + genre_base + key_word + api_key)
+    # genre_req  = requests.get(genres_url)
+
+    # if genre_req:
+    #     print('genres response was :  ',  genre_req.status_code)
+    #     genre_to_json = genre_req.json()
+    #     #dump_gnr = json.dumps(genre_to_json, indent=4) 
+
+    #     # save to jsn file 
+    #     with open('genres.json', 'w') as gnrs:
+    #         json.dump(genre_to_json, gnrs, indent=4, sort_keys=True)
+
+    # else:
+    #     Exception()
+
+    with open('genres.json', 'r') as fetch_genres:
+        
+        #create Function attribute to access outside of this fun
+        get_genres.categ_fetched = json.load(fetch_genres)
+        # print('genres list is  : \n ', json.dumps(categ_fetched, indent=4))
+
+    return dict (
+        categories= get_genres.categ_fetched
+    )
+
 
 # Fetch by categorie 
-def get_by_category():
-    pass
+@stream.route('/catg/<int:genre_id>/<string:id_name>')
+def get_by_category(genre_id, id_name):
+
+    # Visit tmdb api page to discover more of these options 
+    movie_discover = 'discover/movie'
+    sort_by = 'popularity.desc'
+    include_adult = 'true'
+    include_video='true'
+    with_genres = genre_id  #(user clicks btn)
+
+    discover_url = str(global_url+movie_discover+key_word+api_key+language+'&sort_by='+sort_by+
+        '&include_adult='+include_adult+'&include_video='+include_video+'&with_genres='+str(with_genres))
+
+    discover_call =  requests.get(discover_url)
+    discover_to_jsn = discover_call.json()
+
+    print("discover resp :",discover_call.status_code)
+
+    #get clicked id name / save the movies to .json file 
+    for get_name in get_genres.categ_fetched['genres']:
+        if with_genres == get_name['id']:
+            id_name = get_name['name']
+
+    print('you have clicked Genre id Nm %d and it\'s name is %s :' % (with_genres, id_name), 
+    '\n --> .json file is saved')
+
+    with open(id_name+'.json', 'w') as new_list:
+        dicv_res = discover_call.json()
+        json.dump(dicv_res,new_list, indent=4)
+
+    #return dict(discover_to_jsn=discover_to_jsn,with_genres=with_genres)
+
+    #return jsonify({"Message" :"Success"})
+    return render_template('popular_mv.html',
+        with_genres = with_genres,
+        list_by_genre=discover_to_jsn,
+        popular_movies= main.pop_dt,
+        trend_dt=main.trend_data,
+        categories = get_genres.categ_fetched
+    )
+
+
+    # //TODO:DELEte later 
+@stream.route('/test')
+def test():
+    with open('genres.json', 'r') as fetch_genres:
+        categ_fetched = json.load(fetch_genres)
+
+    return render_template('junk.html',
+    categories=categ_fetched)
+
+ 
+    # parse the dict() to extende this context in other routes    
+
+
+    # # get my poppular movies list 
+    # with open('Popular_movies.json','r') as fetch_pop:
+    #     pop_fetched = json.load(fetch_pop)
+
 
 @stream.route('/Popular_Movies/Detail/<string:movie_id>')
 def movie_detail(movie_id):
     append_to = '&append_to_response='
 
-    # Append to this url other details 
+    # Append to url other details 
     credit = 'credits'
     images = 'images'
     videos = 'videos'
@@ -216,14 +278,13 @@ def movie_detail(movie_id):
 
     # get detail for every movie selected 
 
-
     return render_template(
         'detail.html', 
-        mv_detail = movie_js    
+        mv_detail = movie_js   
     )
     
 
-# Delete later
+# //TODO: Delete later
 @stream.route('/imgs')
 def imgs():
     with open('Trending_movies.json', 'r') as f:
